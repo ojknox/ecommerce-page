29da401701ab51b8ab0f637a0e5aec16e22fb02f
OKnox <90188662+ojknox@users.noreply.github.com>
2022-07-22 12:31:32 +0700

    working cart functionality

--- script.js
+++ script.js
@@ -6,119 +6,185 @@ const num = document.querySelector(".num");
 let n = 0;
 
 plus.addEventListener("click", () => {
-    n++;
-    num.innerHTML = n;
+  n++;
+  num.innerHTML = n;
+  updateCartTotal();
 });
 
 minus.addEventListener("click", () => {
-    if(n > 0){
-        n--;
-        num.innerHTML = n;
-    }
-})
+  if (n > 0) {
+    n--;
+    num.innerHTML = n;
+    updateCartTotal();
+  }
+
+  if(!cartItem.classList.contains("inactive") && n === 0){
+    switchCart();
+  }
+});
 
 //Switch the large product image by clicking on the small thumbnail images
-const mainImg = document.getElementById('mainImg'); //big image on normal page
-const thumbnails = document.querySelectorAll('.thumbnailImg'); //thumbnails
+const mainImg = document.getElementById("mainImg"); //big image on normal page
+const thumbnails = document.querySelectorAll(".thumbnailImg"); //thumbnails
 const thumbnailArray = Array.from(thumbnails); //array of thumbnails
-const thumbnailDivs = document.querySelectorAll('.photo.mini');
+const thumbnailDivs = document.querySelectorAll(".photo.mini");
 
-const lastImage = thumbnailArray.length - 1; 
-const lightboxMainImg = document.querySelector('.lightbox-img'); //big image in lightbox
-const lightboxThumbnailDivs = document.querySelectorAll('.thumbnail-container');
+const lastImage = thumbnailArray.length - 1;
+const lightboxMainImg = document.querySelector(".lightbox-img"); //big image in lightbox
+const lightboxThumbnailDivs = document.querySelectorAll(".thumbnail-container");
 
 let activeImage = 0; //starts off with 1st image
 
 const setActiveImage = (image) => {
-    mainImg.src = image.src.replace('-thumbnail', ''); //change main photo when thumbnail clicked
-    lightboxMainImg.src = image.src.replace('-thumbnail', ''); //change lightbox main photo
-    activeImage = thumbnailArray.indexOf(image); //record index of active image
-}
+  mainImg.src = image.src.replace("-thumbnail", ""); //change main photo when thumbnail clicked
+  lightboxMainImg.src = image.src.replace("-thumbnail", ""); //change lightbox main photo
+  activeImage = thumbnailArray.indexOf(image); //record index of active image
+};
 
-thumbnails.forEach(image => {
-    image.addEventListener('click', (e) => { 
-        //change main photo
-        setActiveImage(image);
-        //remove 'selected' thumbnail
-        thumbnailDivs.forEach(div => {
-            div.classList.remove('active');
-        })
-        //change active thumbnail
-        image.parentElement.classList.add('active');
-    })
-})
+thumbnails.forEach((image) => {
+  image.addEventListener("click", (e) => {
+    //change main photo
+    setActiveImage(image);
+    //remove 'selected' thumbnail
+    thumbnailDivs.forEach((div) => {
+      div.classList.remove("active");
+    });
+    //change active thumbnail
+    image.parentElement.classList.add("active");
+  });
+});
 
 //Making lightbox appear
 //Variables
-const lightboxEnabled = document.querySelector('.photo.main'); 
-const lightboxContainer = document.querySelector('.lightbox-container');
-const lightboxBtns = document.querySelectorAll('.lightbox-btn');
-const lightboxBtnLeft = document.getElementById('left');
-const lightboxBtnRight = document.getElementById('right');
-const lightboxThumbnails = document.querySelectorAll('.lightbox-thumbnail-img');
+const lightboxEnabled = document.querySelector(".photo.main");
+const lightboxContainer = document.querySelector(".lightbox-container");
+const lightboxBtns = document.querySelectorAll(".lightbox-btn");
+const lightboxBtnLeft = document.getElementById("left");
+const lightboxBtnRight = document.getElementById("right");
+const lightboxThumbnails = document.querySelectorAll(".lightbox-thumbnail-img");
 const lightboxThumbnailArray = Array.from(lightboxThumbnails);
 
 //Functions of lightbox
 const changeLightboxThumbnail = () => {
-    lightboxThumbnailDivs.forEach(div => {
-        div.classList.remove('active');
-    })
-    lightboxThumbnailArray[activeImage].parentElement.classList.add('active');
-}
-
-const showLightbox = () => {
-    lightboxContainer.classList.add('active');
-    changeLightboxThumbnail();
+  lightboxThumbnailDivs.forEach((div) => {
+    div.classList.remove("active");
+  });
+  lightboxThumbnailArray[activeImage].parentElement.classList.add("active");
 };
 
-const hideLightbox = () => {lightboxContainer.classList.remove('active')};
+const showLightbox = () => {
+  lightboxContainer.classList.add("active");
+  changeLightboxThumbnail();
+};
+
+const hideLightbox = () => {
+  lightboxContainer.classList.remove("active");
+};
 
 const transitionSlideLeft = () => {
-    activeImage === 0 ? setActiveImage(thumbnailArray[lastImage]) : setActiveImage(thumbnailArray[activeImage -1]);
-    changeLightboxThumbnail();
-}
+  activeImage === 0
+    ? setActiveImage(thumbnailArray[lastImage])
+    : setActiveImage(thumbnailArray[activeImage - 1]);
+  changeLightboxThumbnail();
+};
 
 const transitionSlideRight = () => {
-    activeImage === lastImage ? setActiveImage(thumbnailArray[0]) : setActiveImage(thumbnailArray[activeImage +1]);
-    changeLightboxThumbnail();
-}
+  activeImage === lastImage
+    ? setActiveImage(thumbnailArray[0])
+    : setActiveImage(thumbnailArray[activeImage + 1]);
+  changeLightboxThumbnail();
+};
 
 const transitionSlideHandler = (moveItem) => {
-    moveItem.includes('left') ? transitionSlideLeft() : transitionSlideRight();
-}
+  moveItem.includes("left") ? transitionSlideLeft() : transitionSlideRight();
+};
 
 //Event listeners for lightbox
-lightboxEnabled.addEventListener('click', () => {
-    showLightbox();
-})
+lightboxEnabled.addEventListener("click", () => {
+  showLightbox();
+});
 
-lightboxContainer.addEventListener('click', () => {
-    hideLightbox();
-})
+lightboxContainer.addEventListener("click", () => {
+  hideLightbox();
+});
 
-lightboxBtns.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        e.stopPropagation(); //stop container from also being clicked and therefore closed
-        transitionSlideHandler(e.currentTarget.id); //prints left or right
-    })
-})
+lightboxBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.stopPropagation(); //stop container from also being clicked and therefore closed
+    transitionSlideHandler(e.currentTarget.id); //prints left or right
+  });
+});
 
-lightboxThumbnails.forEach(thumbnail => {
-    thumbnail.addEventListener('click', (e) => {
-        e.stopPropagation();
-        setActiveImage(thumbnail);
-        lightboxThumbnailDivs.forEach(div => {
-            div.classList.remove('active');
-        })
-        thumbnail.parentElement.classList.add('active');
-        activeImage = lightboxThumbnailArray.indexOf(thumbnail);
-    })
-})
+lightboxThumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener("click", (e) => {
+    e.stopPropagation();
+    setActiveImage(thumbnail);
+    lightboxThumbnailDivs.forEach((div) => {
+      div.classList.remove("active");
+    });
+    thumbnail.parentElement.classList.add("active");
+    activeImage = lightboxThumbnailArray.indexOf(thumbnail);
+  });
+});
 
-//Event listener to show cart 
-const cart = document.querySelector('.cart');
-const cartContainer = document.querySelector('.cart-container');
+//Event listener to show cart
+const cart = document.querySelector(".cart");
+const cartContainer = document.querySelector(".cart-container");
 
-cart.addEventListener('click', () => {
-    cartContainer.classList.toggle('active');
-})
+cart.addEventListener("click", () => {
+  cartContainer.classList.toggle("active");
+});
+
+//Cart variables
+const deleteButton = document.getElementsByClassName("delete")[0];
+const checkoutButton = document.querySelector(".button-add.checkout");
+const emptyBasket = document.getElementsByClassName("cart-empty")[0];
+const cartItem = document.getElementsByClassName("cart-item")[0];
+const addToCartButton = document.getElementById("addToCart");
+
+//Start with empty basket
+if (n === 0) {
+  emptyBasket.style.display = "block";
+  cartItem.classList.toggle("inactive");
+  checkoutButton.style.display = "none";
+}
+
+//add to cart
+addToCartButton.addEventListener("click", () => {
+  if (cartItem.classList.contains("inactive") === true && n !== 0) {
+    switchCart();
+  }
+});
+
+//Delete item from cart
+deleteButton.addEventListener("click", () => {
+  switchCart();
+});
+
+const changeDisplay = (item) => {
+  if (item.style.display === "none") {
+    item.style.display = "block";
+  } else {
+    item.style.display = "none";
+  }
+};
+
+const switchCart = () => {
+    cartItem.classList.toggle("inactive");
+    changeDisplay(checkoutButton);
+    changeDisplay(emptyBasket);
+}
+
+//Update cart quantity and total
+//n is the qty selected on the page
+const qtyElement = document.querySelector(".qty");
+qtyElement.innerHTML = n;
+const price = 125;
+const totalElement = document.querySelector(".total");
+totalElement.innerHTML = `$${(n * price).toFixed(2)}`;
+//const total = parseFloat(totalElement.innerHTML.replace('$', ''));
+
+const updateCartTotal = () => {
+  qtyElement.innerHTML = n;
+  totalElement.innerHTML = `$${(n * price).toFixed(2)}`;
+};
